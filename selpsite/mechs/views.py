5d60ee1ddc76567aafe09670f58404c8c4de44c0
Lewis Barker <s1236818@pontremoli.inf.ed.ac.uk>
2014-10-16 15:52:40 +0100

    Added a basic Django template

--- selpsite/mechs/views.py
+++ selpsite/mechs/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
+
 from mechs.models import StdMech
 
 def index(request):
-    first_mech_list = StdMech.objects.order_by('name')[:5]
-    output = "<b>Some mechs:</b><br>" + ',<br>'.join([p.name for p in first_mech_list])
-    return HttpResponse(output)
+  first_mech_list = StdMech.objects.order_by('name')[:5]
+  #template = loader.get_template('mechs/index.html')
+  context = {'first_mech_list': first_mech_list}
+  return render(request, 'mechs/index.html', context)
 
 def detail(request, mech_id):
-    return HttpResponse("You're looking at mech %s." % mech_id)
+  return HttpResponse("You're looking at mech %s." % mech_id)
