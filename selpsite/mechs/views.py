8ff8ebdecbf24c389d23389d344d76ff446776f2
Lewis Barker <l.barker.404@gmail.com>
2014-12-02 02:54:50 +0000

    Fixed indentation (4 spaces for python, 2 spaces for HTML)

--- selpsite/mechs/views.py
+++ selpsite/mechs/views.py
@@ -5,12 +5,12 @@ from django.views import generic
 from mechs.models import StdMech
 
 class index(generic.ListView):
-  template_name = 'mechs/index.html'
-  context_object_name = 'first_mech_list'
+    template_name = 'mechs/index.html'
+    context_object_name = 'first_mech_list'
 
-  def get_queryset(self):
-    """Return the first five mechs."""
-    return StdMech.objects.order_by('name')[:5]
+    def get_queryset(self):
+        """Return the first five mechs."""
+        return StdMech.objects.order_by('name')[:5]
 
 class detail(generic.DetailView):
     model = StdMech
--- selpsite/users/urls.py
+++ selpsite/users/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from users import views
 
 urlpatterns = patterns('',
-  url(r'^login/$', views.login, name='login'),
-  url(r'^logout/$', views.logout, name='logout'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
 )
