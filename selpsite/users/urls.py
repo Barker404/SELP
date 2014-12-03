2e4030fb61c8c456092a751e9d970d9ee6043003
Lewis Barker <l.barker.404@gmail.com>
2014-12-03 16:10:32 +0000

    Added "account" page accessible only by logged in users

--- selpsite/users/urls.py
+++ selpsite/users/urls.py
@@ -4,4 +4,5 @@ from users import views
 urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'users/login.html'}),
     url(r'^logout/$', views.logoutView, name='logout'),
+    url(r'^account/$', views.accountView, name='account'),
 )
--- selpsite/users/views.py
+++ selpsite/users/views.py
@@ -11,3 +11,9 @@ def logoutView(request):
     logout(request)
     return HttpResponse("Logged out successfully")
 
+def accountView(request):
+    # Redirect to login page if user is not logged in
+    if not request.user.is_authenticated():
+        return redirect('/login/?next=%s' % request.path)
+    else:
+        return render(request, 'users/account.html', { 'user': request.user })
