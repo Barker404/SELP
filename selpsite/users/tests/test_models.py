021693c380e6389dab297ebef4096725859d6901
Lewis Barker <l.barker.404@gmail.com>
2014-12-08 03:11:19 +0000

    Added model tests for score method

--- selpsite/users/tests/test_models.py
+++ selpsite/users/tests/test_models.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Not much to test yet, only model is very simple with no methods
+from django.contrib.auth.models import User
+from users.models import UserProfile
+
+class UserModelsTestCase(TestCase):
+    fixtures = ['auth_user_testdata', 'users_testdata']
+
+    # def setUp(self):
+    #     user1 = User.objects.create_user('user1', '', 'user1Pass')
+    #     UserProfile.objects.get_or_create(description='user1\'s profile', user=user1)
+
+    def testScore(self):
+        testUser = User.objects.create_user(username='test1', password='test1Pass')
+        profile = UserProfile.objects.create(user=testUser, description='') 
+        self.assertEquals(profile.score(), 0)
+
+        testUser = User.objects.create_user(username='test2', password='test2Pass')
+        profile = UserProfile.objects.create(user=testUser, description='', wins=5) 
+        self.assertEquals(profile.score(), 5)
+        
+        testUser = User.objects.create_user(username='test3', password='test3Pass')
+        profile = UserProfile.objects.create(user=testUser, description='', losses=5) 
+        self.assertEquals(profile.score(), 0)
+
+        testUser = User.objects.create_user(username='test4', password='test4Pass')
+        profile = UserProfile.objects.create(user=testUser, description='', wins=10000, losses=1) 
+        self.assertEquals(profile.score(), 10000)
+
+        testUser = User.objects.create_user(username='test5', password='test5Pass')
+        profile = UserProfile.objects.create(user=testUser, description='', wins=10, losses=100) 
+        self.assertEquals(profile.score(), 10/100)
