ed023a4c144228abdaea257428d39457783aab91
Lewis Barker <l.barker.404@gmail.com>
2014-12-08 22:24:37 +0000

    Added tests for UserProfile.score and its related methods

--- selpsite/users/tests/test_models.py
+++ selpsite/users/tests/test_models.py
@@ -10,7 +10,7 @@ class UserModelsTestCase(TestCase):
     #     user1 = User.objects.create_user('user1', '', 'user1Pass')
     #     UserProfile.objects.get_or_create(description='user1\'s profile', user=user1)
 
-    def testScore(self):
+    def testCalculateScore(self):
         testUser = User.objects.create_user(username='test1', password='test1Pass')
         profile = UserProfile.objects.create(user=testUser, description='') 
         self.assertEquals(profile.calculateScore(), 0)
@@ -30,3 +30,55 @@ class UserModelsTestCase(TestCase):
         testUser = User.objects.create_user(username='test5', password='test5Pass')
         profile = UserProfile.objects.create(user=testUser, description='', wins=10, losses=100) 
         self.assertEquals(profile.calculateScore(), 10/100)
+
+    def testAddWin(self):
+        testUser = User.objects.create_user(username='test1', password='test1Pass')
+        profile = UserProfile.objects.create(user=testUser, description='') 
+        self.assertEquals(profile.score, 0)
+        self.assertEquals(profile.wins, 0)
+        self.assertEquals(profile.losses, 0)
+
+        profile.addWin()
+        self.assertEquals(profile.score, 1)
+        self.assertEquals(profile.wins, 1)
+        self.assertEquals(profile.losses, 0)
+
+        profile.addWin()
+        self.assertEquals(profile.score, 2)
+        self.assertEquals(profile.wins, 2)
+        self.assertEquals(profile.losses, 0)
+
+    def testAddLoss(self):
+        testUser = User.objects.create_user(username='test1', password='test1Pass')
+        profile = UserProfile.objects.create(user=testUser, description='') 
+        self.assertEquals(profile.score, 0)
+        self.assertEquals(profile.wins, 0)
+        self.assertEquals(profile.losses, 0)
+
+        profile.addLoss()
+        self.assertEquals(profile.score, 0)
+        self.assertEquals(profile.wins, 0)
+        self.assertEquals(profile.losses, 1)
+
+        profile.addLoss()
+        self.assertEquals(profile.score, 0)
+        self.assertEquals(profile.wins, 0)
+        self.assertEquals(profile.losses, 2)
+
+    def testScore(self):
+        testUser = User.objects.create_user(username='test1', password='test1Pass')
+        profile = UserProfile.objects.create(user=testUser, description='') 
+        self.assertEquals(profile.score, 0)
+
+        profile.addWin()
+        self.assertEquals(profile.score, 1)
+        profile.addWin()
+        self.assertEquals(profile.score, 2)
+        profile.addLoss()
+        self.assertEquals(profile.score, 2)
+        profile.addLoss()
+        self.assertEquals(profile.score, 1)
+        profile.addLoss()
+        self.assertEquals(profile.score, 2/3)
+
+
