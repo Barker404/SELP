270f5320c960a279385ca97e44d6a0520c045c74
Lewis Barker <l.barker.404@gmail.com>
2014-12-08 03:16:55 +0000

    Made some comments on efficiency of current ranking method
    Hint: it's not good

--- selpsite/users/views.py
+++ selpsite/users/views.py
@@ -51,6 +51,9 @@ class RankingView(generic.ListView):
         return rankedUsers()
 
 def rankedUsers():
+    # This gets the list from the database then sorts it in python
+    # Doesn't scale well, less efficient than sorting in the database
+    # Alternative would require custom SQL however
     unsortedResults = User.objects.all()
     sortedResults = sorted(unsortedResults, key=lambda t: t.profile.score())
     return sortedResults
