a2d60621babfb10dd8b26f16c111071e84378c00
Lewis Barker <l.barker.404@gmail.com>
2014-12-09 00:00:54 +0000

    Removed user creations from model tests to make them faster

--- selpsite/users/tests/test_models.py
+++ selpsite/users/tests/test_models.py
@@ -11,29 +11,26 @@ class UserModelsTestCase(TestCase):
     #     UserProfile.objects.get_or_create(description='user1\'s profile', user=user1)
 
     def testCalculateScore(self):
-        testUser = User.objects.create_user(username='test1', password='test1Pass')
-        profile = UserProfile.objects.create(user=testUser, description='') 
+        profile = User.objects.get(username='lewis').profile
         self.assertEquals(profile.calculateScore(), 0)
 
-        testUser = User.objects.create_user(username='test2', password='test2Pass')
-        profile = UserProfile.objects.create(user=testUser, description='', wins=5) 
+        profile.wins = 5
         self.assertEquals(profile.calculateScore(), 5)
         
-        testUser = User.objects.create_user(username='test3', password='test3Pass')
-        profile = UserProfile.objects.create(user=testUser, description='', losses=5) 
+        profile.wins = 0
+        profile.losses = 5
         self.assertEquals(profile.calculateScore(), 0)
 
-        testUser = User.objects.create_user(username='test4', password='test4Pass')
-        profile = UserProfile.objects.create(user=testUser, description='', wins=10000, losses=1) 
+        profile.wins = 10000
+        profile.losses = 1
         self.assertEquals(profile.calculateScore(), 10000)
 
-        testUser = User.objects.create_user(username='test5', password='test5Pass')
-        profile = UserProfile.objects.create(user=testUser, description='', wins=10, losses=100) 
+        profile.wins = 10
+        profile.losses = 100
         self.assertEquals(profile.calculateScore(), 10/100)
 
     def testAddWin(self):
-        testUser = User.objects.create_user(username='test1', password='test1Pass')
-        profile = UserProfile.objects.create(user=testUser, description='') 
+        profile = User.objects.get(username='lewis').profile
         self.assertEquals(profile.score, 0)
         self.assertEquals(profile.wins, 0)
         self.assertEquals(profile.losses, 0)
@@ -49,8 +46,7 @@ class UserModelsTestCase(TestCase):
         self.assertEquals(profile.losses, 0)
 
     def testAddLoss(self):
-        testUser = User.objects.create_user(username='test1', password='test1Pass')
-        profile = UserProfile.objects.create(user=testUser, description='') 
+        profile = User.objects.get(username='lewis').profile
         self.assertEquals(profile.score, 0)
         self.assertEquals(profile.wins, 0)
         self.assertEquals(profile.losses, 0)
@@ -66,8 +62,7 @@ class UserModelsTestCase(TestCase):
         self.assertEquals(profile.losses, 2)
 
     def testScore(self):
-        testUser = User.objects.create_user(username='test1', password='test1Pass')
-        profile = UserProfile.objects.create(user=testUser, description='') 
+        profile = User.objects.get(username='lewis').profile
         self.assertEquals(profile.score, 0)
 
         profile.addWin()
