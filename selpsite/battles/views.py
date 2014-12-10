0a1bf99fd677d960ce7243910714425b26412870
Lewis Barker <l.barker.404@gmail.com>
2014-12-10 01:39:48 +0000

    Added a simple "start battle" view with no functionality

--- selpsite/battles/views.py
+++ selpsite/battles/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def startBattleView(request):
+    return render(request, 'battles/startBattle.html')
--- selpsite/selpsite/urls.py
+++ selpsite/selpsite/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', include('home.urls')),
     url(r'^mechs/', include('mechs.urls')),
     url(r'^users/', include('users.urls')),
+    url(r'^battle/', include('battles.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
