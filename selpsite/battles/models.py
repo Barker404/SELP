0b54e75e7d7551e4621a897a71e83933352a3a2a
Lewis Barker <l.barker.404@gmail.com>
2014-12-13 19:50:39 +0000

    Reorganised battle models to allow for foreign key references

--- selpsite/battles/models.py
+++ selpsite/battles/models.py
@@ -2,6 +2,56 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Represents a "move" in a battle made by a single player
+class Move(models.Model):
+    # More moves to be added if time
+    # Potentially "move backwards", "move forwards" etc
+    # May also have some concept of foreign key to 
+    # "Possible Move"/"Attack" object
+    ROCK = 'R'
+    PAPER = 'P'
+    SCISSORS = 'S'
+    MOVE_CHOICES = (
+        (ROCK, 'Rock'),
+        (PAPER, 'Paper'),
+        (SCISSORS, 'Scissors'),
+    )
+    moveUsed = models.CharField(max_length=1,
+                                 choices=MOVE_CHOICES)
+    player = models.ForeignKey('Player')
+    moveNo = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
+
+
+# Represents one of the "players" in a single game
+# Different from a user in that it represents the player's information
+# for just the one game
+class Player(models.Model):
+    MAX_HP = 100
+    user = models.ForeignKey(User)
+    hp = models.IntegerField(default=MAX_HP)
+    # "Locked in" moves - the player can no longer chagne them
+    # When both are not null, the turn can proceed
+    currentMove = models.ForeignKey(Move, 
+                                    related_name='currentMove', 
+                                    null=True)
+    opponent = models.OneToOneField('self',
+                                    related_name='_opponent',
+                                    null=True)
+    # Saves the current user as their opponent's opponent
+    def save(self, *args, **kwargs):
+        super(Player, self).save()
+        self.opponent.opponent = self
+
+    def makeMove(self, moveUsed):
+        # if (self.currentMove == null)
+        move = Move.objects.create(moveUsed = moveUsed,
+                                   player = self,
+                                   moveNo = self.battle.turnNumber)
+        self.currentMove = move
+        move.save()
+        self.save()
+
 # Represents a "battle" between two players
 class Battle(models.Model):
     WAITING_FOR_PLAYER = 1
@@ -35,53 +85,3 @@ class Battle(models.Model):
     # Store the time of the most recent move so that we know if the
     # players are inactive
     lastMoveTime = models.DateTimeField(auto_now_add=True)
-
-
-# Represents one of the "players" in a single game
-# Different from a user in that it represents the player's information
-# for just the one game
-class Player(models.Model):
-    MAX_HP = 100
-    user = models.ForeignKey(User)
-    hp = models.IntegerField(default=MAX_HP)
-    # "Locked in" moves - the player can no longer chagne them
-    # When both are not null, the turn can proceed
-    currentMove = models.ForeignKey(Move, 
-                                    related_name='currentMove', 
-                                    null=True)
-    opponent = models.OneToOneField('self',
-                                    related_name='_opponent',
-                                    null=True)
-    # Saves the current user as their opponent's opponent
-    def save(self, *args, **kwargs):
-        super(Player, self).save()
-        self.opponent.opponent = self
-
-    def makeMove(self, moveUsed):
-        if (self.currentMove == null)
-        move = Move.objects.create(moveUsed = moveUsed,
-                                   player = self,
-                                   moveNo = self.battle.turnNumber)
-        self.currentMove = move
-        move.save()
-        self.save()
-
-# Represents a "move" in a battle made by a single player
-class Move(models.Model):
-    # More moves to be added if time
-    # Potentially "move backwards", "move forwards" etc
-    # May also have some concept of foreign key to 
-    # "Possible Move"/"Attack" object
-    ROCK = 'R'
-    PAPER = 'P'
-    SCISSORS = 'S'
-    MOVE_CHOICES = (
-        (ROCK, 'Rock'),
-        (PAPER, 'Paper'),
-        (SCISSORS, 'Scissors'),
-    )
-    moveUsed = models.CharField(max_length=1,
-                                 choices=MOVES)
-    player = models.ForeignKey(Player)
-    moveNo = models.IntegerField()
-    time = models.DateTimeField(auto_now_add=True)
