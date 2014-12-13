16903f8c97f53b5032b3d2967df2d267b98f8513
Lewis Barker <l.barker.404@gmail.com>
2014-12-13 19:48:20 +0000

    [WIP] Added a player model and some new fields
    The "player" object is tied to a specific game, unlike a user object.

--- selpsite/battles/models.py
+++ selpsite/battles/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Represents a "battle" between two players
 class Battle(models.Model):
     WAITING_FOR_PLAYER = 1
@@ -13,52 +14,74 @@ class Battle(models.Model):
         (CALCULATING, 'Calculating results'),
         (FINISHED, 'Battle finished'),
     )
-    startTime = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUSES,
                                  default=WAITING_FOR_PLAYER)
+    startTime = models.DateTimeField(auto_now_add=True)
 
-    # Definitely need to have a better concept of "player1" and "player2"
-    # Either some kind of indexing/tuples
-    # Or seperate "player" obejcts containing the HP, current move, etc.
-    player1 = models.ForeignKey(User, 
-                                related_name='player1', 
-                                null=True)
-    player2 = models.ForeignKey(User, 
-                                related_name='player2', 
-                                null=True)
+    # Links to the two player objects. 
+    # Will be null before players connect
+    player1 = models.OneToOneField(Player,
+                                   related_name='player1', 
+                                   null=True)
+    player2 = models.OneToOneField(Player,
+                                   related_name='player2', 
+                                   null=True)
     # Includes the current turn
     turnNumber = models.IntegerField(default=0)
-    winner = models.ForeignKey(User, 
+    # Will be null during the game
+    winner = models.ForeignKey(Player, 
                                related_name='winner', 
                                null=True)
-    player1HP = models.IntegerField()
-    player2HP = models.IntegerField()
+    # Store the time of the most recent move so that we know if the
+    # players are inactive
+    lastMoveTime = models.DateTimeField(auto_now_add=True)
+
+
+# Represents one of the "players" in a single game
+# Different from a user in that it represents the player's information
+# for just the one game
+class Player(models.Model):
+    MAX_HP = 100
+    user = models.ForeignKey(User)
+    hp = models.IntegerField(default=MAX_HP)
     # "Locked in" moves - the player can no longer chagne them
     # When both are not null, the turn can proceed
-    player1CurrentMove = models.ForeignKey(User, 
-                                           related_name='player1CurrentMove', 
-                                           null=True)
-    player2CurrentMove = models.ForeignKey(User, 
-                                           related_name='player2CurrentMove', 
-                                           null=True)
+    currentMove = models.ForeignKey(Move, 
+                                    related_name='currentMove', 
+                                    null=True)
+    opponent = models.OneToOneField('self',
+                                    related_name='_opponent',
+                                    null=True)
+    # Saves the current user as their opponent's opponent
+    def save(self, *args, **kwargs):
+        super(Player, self).save()
+        self.opponent.opponent = self
+
+    def makeMove(self, moveUsed):
+        if (self.currentMove == null)
+        move = Move.objects.create(moveUsed = moveUsed,
+                                   player = self,
+                                   moveNo = self.battle.turnNumber)
+        self.currentMove = move
+        move.save()
+        self.save()
 
 # Represents a "move" in a battle made by a single player
 class Move(models.Model):
     # More moves to be added if time
     # Potentially "move backwards", "move forwards" etc
-    # May also have some concept of foreign key to "Possible Move"/"Attack" object
+    # May also have some concept of foreign key to 
+    # "Possible Move"/"Attack" object
     ROCK = 'R'
     PAPER = 'P'
     SCISSORS = 'S'
-    MOVES = (
+    MOVE_CHOICES = (
         (ROCK, 'Rock'),
         (PAPER, 'Paper'),
         (SCISSORS, 'Scissors'),
     )
-    move_used = models.CharField(max_length=1,
-                                choices=MOVES,
-                                default=ROCK)
-    player = models.ForeignKey(User)
-    # Somehow restrict to 1 or 2? 
-    playerNo = models.IntegerField()
+    moveUsed = models.CharField(max_length=1,
+                                 choices=MOVES)
+    player = models.ForeignKey(Player)
     moveNo = models.IntegerField()
+    time = models.DateTimeField(auto_now_add=True)
