dac264ff15fffe7877569a8ad3c0472107e356f1
Lewis Barker <l.barker.404@gmail.com>
2014-12-14 02:20:41 +0000

    Added basic home view test, plus stub files for other tests

---
+++ selpsite/home/tests/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+class UsersViewsTestCase(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
