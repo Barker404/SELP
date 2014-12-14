8e0a9483f411a2db302131f696006f68560561eb
Lewis Barker <l.barker.404@gmail.com>
2014-12-14 10:34:19 +0000

    Added status resetting to joinBattle in views

--- selpsite/battles/views.py
+++ selpsite/battles/views.py
@@ -10,25 +10,37 @@ def startBattleView(request):
 # If true the player should wait for another to join the game
 # If false, the player should retry until success
 def joinBattle(player):
+    # Get all battles with space
     spaces = Battle.objects.filter(Q(player1__isnull=True,
                                      player2__isnull=False,
-                                     status=WAITING_FOR_PLAYERS) |
+                                     status=Battle.WAITING_FOR_PLAYERS) |
                                    Q(player1__isnull=False,
                                      player2__isnull=True,
-                                     status=WAITING_FOR_PLAYERS))
+                                     status=Battle.WAITING_FOR_PLAYERS))
 
     if (spaces):
         # (sort-of) prevent other players trying to join
-        # Obvious race condition, might need to fix later
+        # Still obvious race condition, might need to fix later
         game = spaces.first()
         game.status = CALCULATING
         game.save()
 
+        # Model method handles actually joining
         success = game.tryAddPlayer(player)
         if (success):
+            # Reset the status
+            if (bool(game.player1) and bool(game.player2)):
+                game.status = Battle.WAITING_FOR_CHOICE
+            else:
+                game.status = Battle.WAITING_FOR_PLAYERS
             return True
         else:
+            # Need to properly define what to do with status here
+            # In theory if the join was unsuccessful because there's 
+            # another joined player, they should set the status 
+            # afterwards.
             return False
     else:
+        # No spaces in existing games, so make a new one!
         Battle.objects.create(player1=player)
         return True
