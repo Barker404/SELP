60d86d2a0a1acbfbb490388e34cfc88b577bef8d
Lewis Barker <l.barker.404@gmail.com>
2014-12-14 08:47:17 +0000

    Moved lookup of user and player objects out to class variables

--- selpsite/battles/tests/test_models.py
+++ selpsite/battles/tests/test_models.py
@@ -6,42 +6,41 @@ from django.contrib.auth.models import User
 from battles.models import Move, Player, Battle
 
 class BattlesViewsTestCase(TestCase):
-    fixtures = ['auth_user_testdata']
+    fixtures = ['auth_user_testdata', 'battles_player_testdata']
+
+    def setUp(self):
+        self.user1 = User.objects.get(username='user1')
+        self.user2 = User.objects.get(username='lewis')
+        self.user3 = User.objects.get(username='staff')
+        self.player1 = Player.objects.get(user=self.user1)
+        self.player2 = Player.objects.get(user=self.user2)
+        self.player3 = Player.objects.get(user=self.user3)
 
     def test_player_save(self):
-        print (User.objects.all())
-        user1 = User.objects.get(username='user1')
-        user2 = User.objects.get(username='lewis')
-        user3 = User.objects.get(username='staff')
-        player1 = Player.objects.create(user=user1)
-        player2 = Player.objects.create(user=user2)
-        player3 = Player.objects.create(user=user3)
-        self.assertIsNone(player1.opponent)
-        self.assertIsNone(player2.opponent)
-        self.assertIsNone(player3.opponent)
-        player1.opponent = player2
-        player1.save()
-        self.assertEqual(player2.opponent, player1)
-        player1.opponent = None
-        player1.save()
-        player3.opponent = player2
-        player3.save()
-        self.assertEqual(player2.opponent, player3)
+        print (self.player1)
+        self.assertIsNone(self.player1.opponent)
+        self.assertIsNone(self.player2.opponent)
+        self.assertIsNone(self.player3.opponent)
+        self.player1.opponent = self.player2
+        self.player1.save()
+        self.assertEqual(self.player2.opponent, self.player1)
+        self.player1.opponent = None
+        self.player1.save()
+        self.player3.opponent = self.player2
+        self.player3.save()
+        self.assertEqual(self.player2.opponent, self.player3)
 
     def test_add_move(self):
-        user1 = User.objects.get(username='user1')
-        player1 = Player.objects.create(user=user1)
-        battle = Battle.objects.create(player1=player1)
+        battle = Battle.objects.create(player1=self.player1)
         
         self.assertEqual(Move.objects.count(), 0)
-        move = player1.addMove(Move.ROCK)
+        move = self.player1.addMove(Move.ROCK)
 
         self.assertEqual(Move.objects.count(), 1)
         self.assertEqual(Move.objects.first(), move)
         self.assertEqual(move.moveUsed, Move.ROCK)
-        self.assertEqual(move.player, player1)
+        self.assertEqual(move.player, self.player1)
         self.assertEqual(move.moveNo, 1)
-        self.assertEqual(player1.currentMove, move)
+        self.assertEqual(self.player1.currentMove, move)
         self.assertEqual(battle.lastMoveTime, move.time)
-
-
+        
