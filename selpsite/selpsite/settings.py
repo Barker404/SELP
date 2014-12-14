a57f9d69ac53ea97343dad39eb19be1e54dc4f6d
Lewis Barker <l.barker.404@gmail.com>
2014-12-14 06:56:45 +0000

    Updated settings so test coverage is for battles

--- selpsite/selpsite/settings.py
+++ selpsite/selpsite/settings.py
@@ -66,7 +66,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=users',
+    '--cover-package=battles',
 ]
 
 
