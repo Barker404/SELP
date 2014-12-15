f8d0cc889eda2c2409483fce3e636d1e7da49a21
Lewis Barker <l.barker.404@gmail.com>
2014-12-15 06:28:18 +0000

    Added tests for bad input of chooseMove

--- selpsite/battles/tests/test_views.py
+++ selpsite/battles/tests/test_views.py
@@ -57,3 +57,34 @@ class BattlesViewsTestCase_Join(TestCase):
         self.assertEqual(battle.player2, self.player2)
         self.assertEqual(battle.player1, self.player1)
         self.assertEqual(battle.status, Battle.WAITING_FOR_CHOICE)
+
+class BattlesViewsTestCase_Turns(TestCase):
+    fixtures = ['auth_user_testdata', 
+                'battles_player_testdata', 
+                'battles_battle_testdata']
+
+    def setUp(self):
+        self.user1 = User.objects.get(username='user1')
+        self.user2 = User.objects.get(username='lewis')
+        self.user3 = User.objects.get(username='staff')
+        self.player1 = Player.objects.get(user=self.user1)
+        self.player2 = Player.objects.get(user=self.user2)
+        self.player3 = Player.objects.get(user=self.user3)
+        self.battle1 = Battle.objects.first()
+
+    def test_choose_move_bad_player(self):
+        self.assertEqual(self.player3.move_set.count(), 0)
+        success = self.chooseMove(self.player3, 'R')
+        self.assertFalse(success)
+        self.assertEqual(self.player3.move_set.count(), 0)
+
+    def test_choose_move_bad_game(self):
+        self.battle1.status = Battle.FINISHED
+        self.battle1.save()
+        self.assertEqual(self.player2.move_set.count(), 0)
+        success = self.chooseMove(self.player2, 'R')
+        self.assertFalse(success)
+        self.assertEqual(self.player2.move_set.count(), 0)
+        self.assertEqual(self.battle1.status, Battle.FINISHED)
+
+    
