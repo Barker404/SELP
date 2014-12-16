e528afaf09448592f1512207a8a48daa55994a7c
Lewis Barker <l.barker.404@gmail.com>
2014-12-16 19:16:02 +0000

    Created a new JS file with skeleton methods for playing a battle

---
+++ selpsite/static/js/battle.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('#startBattle').click(function() {
+        startBattle()
+    });
+});
+
+// AJAX calls required:
+// 1. POST (?) trying to join a game
+// 2. GET status
+// 3. POST move choice
+    
+function startBattle() {
+    // gonna loop
+    // Try to join a game, keep trying if it fails
+}
+
+function doBattle() {
+    // more looping
+    // keep checking that game status, update screen when it changes
+    // Every cycle might as well update the hp etc
+    // When "status" actually changes, then show new parts of screen
+
+    // There will be 3 states:
+    // Choosing a move
+    // Waiting for the other player/server calculation
+    // Game finished
+ 
+}
