7d848da3dfc5538ddef92eae2f4871f784c3e883
Lewis Barker <l.barker.404@gmail.com>
2014-12-16 19:57:25 +0000

    Added URL for ajaxGetBattleStatusView

--- selpsite/battles/urls.py
+++ selpsite/battles/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from battles import views
 
 urlpatterns = patterns('',
+    url(r'getBattleStatus', views.ajaxGetBattleStatusView, name='getBattleStatus'),
     url(r'^$', views.startBattleView, name='startBattle'),
 )
