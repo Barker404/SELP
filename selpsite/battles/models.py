d54712c30298746df6d375eb18739fb1cbcedfb9
Lewis Barker <l.barker.404@gmail.com>
2014-12-16 18:33:12 +0000

    Added some temp methods to the Player model
    These should really be fields. Will replace with fields later, and have
    them updated from Battle.save()

--- selpsite/battles/models.py
+++ selpsite/battles/models.py
@@ -52,6 +52,51 @@ class Player(models.Model):
         super(Player, self).save()
         if (not self.opponent is None):
             self.opponent.opponent = self
+
+    # Returns boolean indicating if the player is either player1 or
+    # player2 of some battle
+    # TODO: This should be a field which is automatically updated in
+    # Battle.save()
+    def isInBattle(self):
+        # The attributes to check area related_names for the OneToOne
+        # relations player1 and player2 of Battle
+        # Therefore the attribute might not even exist, so we check
+        # this first
+        return
+            ((hasattr(self, 'player1') and 
+            not self.player1 is None)) or 
+            ((hasattr(self, 'player2') and
+            not self.player2 is None))
+
+    # Returns either 1 or 2 based on the player number in the related
+    # Battle object, or 0 if the player is not in any Battle
+    # TODO: This should be a field which is automatically updated in
+    # Battle.save()
+    def getPlayerNumber(self):
+        if (hasattr(self, 'player1') and 
+            not self.player1 is None):
+            return 1
+        elif (hasattr(self, 'player2') and
+            not self.player2 is None):
+            return 2
+        else:
+            return 0
+
+    # Returns the battle which the player is in, or None if they are 
+    # not in any
+    # TODO: This should be a field which is automatically updated in
+    # Battle.save()
+    def getBattle(self):
+        if (hasattr(self, 'player1') and 
+            not self.player1 is None):
+            return self.player1
+        elif (hasattr(self, 'player2') and
+            not self.player2 is None):
+            return self.player2
+        else:
+            return None
+
+
     # This method is only for adding the move, it contains no logic for
     # if a move can be made at this time, or the effect of it
     def addMove(self, moveUsed):
