3d824fadd63330bc6137066bb734dbc8d530b02c
Lewis Barker <l.barker.404@gmail.com>
2014-12-17 07:09:15 +0000

    [Refactoring] Updated chooseMove to use player.getPlayerNumber and player.getBattle

--- selpsite/battles/views.py
+++ selpsite/battles/views.py
@@ -145,16 +145,10 @@ def joinBattle(player):
 
 def chooseMove(player, move):
     # Check the player is in a battle
-    if (hasattr(player, 'player1') and 
-        not player.player1 is None):
-        battle = player.player1
-        playerNo = 1
-    elif (hasattr(player, 'player2') and
-        not player.player2 is None):
-        battle = player.player2
-        playerNo = 2
-    else:
+    playerNo = player.getPlayerNumber()
+    if (playerNo == 0):
         return False
+    battle = player.getBattle()
 
     # Check that the battle is waiting for the player to choose a 
     # move
