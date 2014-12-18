5d88d4a467bff3350f0a1a95e777059e4b4c4cd9
Barker404 <l.barker.404@gmail.com>
2014-12-18 06:21:41 +0000

    Added flag for runtests to show line numbers in coverage report

--- runtests.sh
+++ runtests.sh
@@ -4,4 +4,4 @@ source selpenv/bin/activate
 # Run the tests - omit coverage on all migrations and on the mechs app (unused code) 
 coverage run --source='selpsite' --omit=*migrations*,*mechs/* selpsite/manage.py test selpsite/
 # Show the report
-coverage report
+coverage report -m
