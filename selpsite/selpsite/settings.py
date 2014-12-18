07f34a2d0126fd415e33a7d7ce9c7636e9ce442c
Barker404 <l.barker.404@gmail.com>
2014-12-18 06:05:31 +0000

    Changed settings to run without using nose for coverage - will use coverage.py instead

--- selpsite/selpsite/settings.py
+++ selpsite/selpsite/settings.py
@@ -60,15 +60,6 @@ ROOT_URLCONF = 'selpsite.urls'
 
 WSGI_APPLICATION = 'selpsite.wsgi.application'
 
-# Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=battles',
-]
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
